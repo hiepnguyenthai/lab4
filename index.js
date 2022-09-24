0a19d1c05d7a984be924de3b3b4d4a48b1fd809a
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-09-24 16:16:05 +0700

    ok

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 http
   .createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.write("<h1>new code here</h1>");
-    res.end("<h2>Heroku deployment tutorial</h2>");
-    res.write("<h3>Node monitor: Tự động theo dõi cập nhật code</h3>");
+    if (req.url === "/") {
+        fs.readFile("./index.html", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.write(data);
+                res.end;
+            }
+        });
+    } else if (req.url === "/greenwich") {
+        fs.readFile("./greenwich.html", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.write(data);
+                res.end;
+            }
+        });
+    } else if (req.url === "/clould") {
+        fs.readFile("./cloud.html", (err, data) => {
+            res.write(data);
+            res.end;
+        });
+    } else {
+        res.end("<h1>404 : Page not found</h1>");
+    }
   })
   .listen(process.env.PORT || 3000);
