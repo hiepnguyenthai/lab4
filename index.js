4c494e0f5eba8fe45b402fdb30e5cd97d766db11
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-09-24 15:11:53 +0700

    ok

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ http
   .createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Heroku deployment tutorial</h1>");
+    res.write("<h1>new code here</h1>");
+    res.end("<h2>Heroku deployment tutorial</h2>");
+    res.write("<h3>Node monitor: Tự động theo dõi cập nhật code</h3>");
   })
   .listen(process.env.PORT || 3000);
